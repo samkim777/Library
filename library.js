92143068b28b1389c3da89b964aca317bf9dcd55
Sam <milk3531@hotmail.com>
2021-08-17 18:38:06 -0700

    Basic functionality for displaying book information

--- library.js
+++ library.js
@@ -9,7 +9,7 @@ function Book(title, author, pages, finished) {
 }
 
 Book.prototype.info = function() {
-    return (this.title + ',' + this.author + ',' + this.pages + ',' + this.finished)
+    return (this.title + ',' + this.author + ',' + this.pages + ',' + this.finished);
 }
 
 
@@ -17,18 +17,23 @@ function addBooktoLibrary(Book) {
     library.push(Book)
 }
 
-let display = document.querySelectorAll('#container')
-let harry_potter = new Book('Harry Potter', 'JK Rowling', '3005', true)
+let display = document.getElementById('container')
+let harry_potter = new Book('Harry Potter', 'JK Rowling', '3005', true);
+let alchemist = new Book('Alchemist', 'Paulo Coelho', '300', false);
 addBooktoLibrary(harry_potter);
+addBooktoLibrary(alchemist);
+display.textContent = 'qwdjiqwdj';
 
 for (let i = 0 ; i < library.length ; i++) {
-    console.log(library[i].info());
+    console.log((library[i].info().toString()));
+    display.textContent = library[i].info().toString();
 }
 
 
 
 
 // Debugging on: 
-// info logging 
-// textContent not showing
+// info logging Check
+// textContent not showing -> Because textcontent needs string but library[i].info is giving a function
+// toString makes function into a string, but is it the correct string? Object undefined this.toString() works
 // displaying books / titles on the page
