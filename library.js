4243a493605161ba621e9562ef45bb1dfb40dab3
Sam <milk3531@hotmail.com>
2021-08-22 16:04:47 -0700

    layout of new book form complete

--- library.js
+++ library.js
@@ -18,15 +18,34 @@ function addBooktoLibrary(Book) {
 }
 
 let content = document.getElementById('grid-container');
+let grid_item = document.getElementById('grid-item');
+let modal = document.getElementById('myModal');
+let btn = document.getElementById('mybtn');
+let span = document.getElementsByClassName('close')[0];
+
 let harry_potter = new Book('Harry Potter', 'JK Rowling', '3005', true);
 let alchemist = new Book('Alchemist', 'Paulo Coelho', '300', false);
 addBooktoLibrary(harry_potter);
 addBooktoLibrary(alchemist);
 
+btn.onclick = function () {
+    modal.style.display = 'block';
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
 
 for (let i = 0; i < library.length; i++) {
     content.children[i].textContent = library[i].info().toString();
-
 }
 
 
@@ -35,6 +54,6 @@ for (let i = 0; i < library.length; i++) {
 
 // Debugging on: 
 
-// Making flex box/ grid boxes and putting textContent inside them
-// Making new books on button event
-// displaying books / titles on the page
+// Add a create new book button 
+// Add a delete book button
+// Add a change status to read button
